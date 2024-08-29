929eb4a7a7289da6456cdaecbf5b6783b3e0d7e5
ciurte-tudor-freelance <ciurte.tudor.freelance@gmail.com>
2024-08-29 21:53:24 +0000

    Added and configured TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: "tw-",
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
